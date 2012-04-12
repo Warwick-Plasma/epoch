6e01dc7b151a2a360fa9e447028ab5c3f9fe1cd3
Keith Bennett <K.Bennett@warwick.ac.uk>
2012-04-12 15:02:38 +0100

    Separated out sdf_find_block routines.

---
+++ VisIt/sdf_util.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include "sdf.h"
+
+
+sdf_block_t *sdf_find_block_by_id(sdf_file_t *h, const char *id)
+{
+    sdf_block_t *current, *b;
+    int i, len;
+
+    if (!h || !h->blocklist || !id)
+        return NULL;
+
+    current = h->blocklist;
+    len = strlen(id) + 1;
+    for (i=0; i < h->nblocks; i++) {
+        b = current;
+        if (memcmp(id, b->id, len) == 0) return b;
+        current = b->next;
+    }
+
+    return NULL;
+}
+
+
+sdf_block_t *sdf_find_block_by_name(sdf_file_t *h, const char *name)
+{
+    sdf_block_t *current, *b;
+    int i, len;
+
+    if (!h || !h->blocklist || !name)
+        return NULL;
+
+    current = h->blocklist;
+    len = strlen(name) + 1;
+    for (i=0; i < h->nblocks; i++) {
+        b = current;
+        if (memcmp(name, b->name, len) == 0) return b;
+        current = b->next;
+    }
+
+    return NULL;
+}
