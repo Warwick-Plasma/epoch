8bf9ff74825b59ae5de25a8c0ba6118d42b65d12
Keith Bennett <K.Bennett@warwick.ac.uk>
2012-04-20 14:35:28 +0100

    Fixed the python SDF module's build script.

--- VisIt/setup.py
+++ VisIt/setup.py
@@ -1,17 +1,22 @@
-import os
+import os, sys
 from distutils.sysconfig import get_python_lib
 from distutils.core import setup, Extension
 
-def getname():
-  for r,d,fl in os.walk(get_python_lib()):
+def get_numpy_dir():
+  for r,d,fl in os.walk(get_python_lib(standard_lib=1)):
     for f in fl:
       if f == 'arrayobject.h':
         return os.path.realpath(os.path.join(r,'..'))
-  return NULL
+  print 'Unable to build python module. Numpy directory not found.'
+  sys.exit(1)
 
-incdir=getname()
+
+sdffiles = ['sdf_control.c', 'sdf_input.c', 'sdf_input_cartesian.c',
+            'sdf_input_point.c']
+
+srcfiles = ['sdf_python.c'] + sdffiles
+
+incdirs = [get_numpy_dir()]
 
 setup(name="sdf", version="1.0",
-      ext_modules=[Extension("sdf", ["sdf_python.c", "sdf_control.c",
-      "sdf_input.c", "sdf_input_cartesian.c", "sdf_input_point.c"],
-      include_dirs=[incdir])])
+      ext_modules=[Extension("sdf", srcfiles, include_dirs=incdirs)])
