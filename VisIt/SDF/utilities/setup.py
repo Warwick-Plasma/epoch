a5ed79b2dd454b4a81743599642e921a173e6b95
Keith Bennett <k.bennett@warwick.ac.uk>
2012-12-11 20:54:38 +0000

    Search more paths when building the python module.
    Also made the build slightly quieter.

--- VisIt/SDF/utilities/setup.py
+++ VisIt/SDF/utilities/setup.py
@@ -3,10 +3,12 @@ from distutils.sysconfig import get_python_lib
 from distutils.core import setup, Extension
 
 def get_numpy_dir():
-  for r,d,fl in os.walk(get_python_lib(standard_lib=1)):
-    for f in fl:
-      if f == 'arrayobject.h':
-        return os.path.realpath(os.path.join(r,'..'))
+  sys.path.insert(0,get_python_lib(standard_lib=1))
+  for path in sys.path:
+    for r,d,fl in os.walk(path):
+      for f in fl:
+        if f == 'arrayobject.h':
+          return os.path.realpath(os.path.join(r,'..'))
   print 'Unable to build python module. Numpy directory not found.'
   sys.exit(1)
 
