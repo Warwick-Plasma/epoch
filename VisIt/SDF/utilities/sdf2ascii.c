b74934124db093b19660f4da84a751ce8441af50
Keith Bennett <K.Bennett@warwick.ac.uk>
2014-02-02 01:09:03 +0000

    sdf2ascii now accepts a range of id's to include.

--- VisIt/SDF/utilities/sdf2ascii.c
+++ VisIt/SDF/utilities/sdf2ascii.c
@@ -23,6 +23,12 @@ struct id_list {
   struct id_list *next;
 } *variable_ids, *last_id;
 
+int nrange;
+
+struct range_type {
+  unsigned int start, end;
+} *range_list;
+
 
 void usage(int err)
 {
@@ -44,10 +50,20 @@ void usage(int err)
 }
 
 
+int range_sort(const void *v1, const void *v2)
+{
+    struct range_type *a = (struct range_type *)v1;
+    struct range_type *b = (struct range_type *)v2;
+
+    return (a->start - b->start);
+}
+
+
 char *parse_args(int *argc, char ***argv)
 {
-    char *file = NULL;
-    int c, err;
+    char *ptr, *file = NULL;
+    int c, i, err, range, sz, nbuf;
+    struct range_type *range_tmp;
     struct stat statbuf;
     static struct option longopts[] = {
         { "no-metadata", no_argument,    NULL,      'n' },
@@ -64,8 +80,11 @@ char *parse_args(int *argc, char ***argv)
     contents = single = use_mmap = ignore_summary = 0;
     variable_ids = NULL;
     last_id = NULL;
+    nbuf = nrange = 0;
+    sz = sizeof(struct range_type);
 
-    while ((c = getopt_long(*argc, *argv, "hncsmiv:", longopts, NULL)) != -1) {
+    while ((c = getopt_long(*argc, *argv,
+            "hncsmiv:", longopts, NULL)) != -1) {
         switch (c) {
         case 'h':
             usage(0);
@@ -80,15 +99,57 @@ char *parse_args(int *argc, char ***argv)
             single = 1;
             break;
         case 'v':
-            if (!variable_ids) {
-                last_id = variable_ids = malloc(sizeof(*variable_ids));
+            if (*optarg >= '0' && *optarg <= '9') {
+                ptr = optarg;
+                range = 0;
+                while (ptr < optarg + strlen(optarg) + 1) {
+                    if (range) {
+                        i = (int)strtol(ptr, &ptr, 10);
+                        if (i == 0)
+                            range_list[nrange-1].end = -1;
+                        else if (i < range_list[nrange-1].start)
+                            nrange--;
+                        else
+                            range_list[nrange-1].end = i;
+                        range = 0;
+                    } else {
+                        nrange++;
+                        // Grow array if necessary
+                        if (nrange > nbuf) {
+                            if (nbuf == 0) {
+                                nbuf = 128;
+                                range_list = malloc(nbuf * sz);
+                            } else {
+                                i = 2 * nbuf;
+
+                                range_tmp = malloc(i * sz);
+                                memcpy(range_tmp, range_list, nbuf * sz);
+                                free(range_list);
+                                range_list = range_tmp;
+
+                                nbuf = i;
+                            }
+                        }
+
+                        i = (int)strtol(ptr, &ptr, 10);
+                        range_list[nrange-1].start = i;
+                        range_list[nrange-1].end = i;
+                        if (*ptr == '-') range = 1;
+                    }
+
+                    ptr++;
+                }
             } else {
-                last_id->next = malloc(sizeof(*variable_ids));
-                last_id = last_id->next;
+                if (!variable_ids) {
+                    last_id = variable_ids = malloc(sizeof(*variable_ids));
+                } else {
+                    last_id->next = malloc(sizeof(*variable_ids));
+                    last_id = last_id->next;
+                }
+                last_id->next = NULL;
+                last_id->id = malloc(strlen(optarg)+1);
+                memcpy(last_id->id, optarg, strlen(optarg)+1);
             }
-            last_id->next = NULL;
-            last_id->id = malloc(strlen(optarg)+1);
-            memcpy(last_id->id, optarg, strlen(optarg)+1);
             break;
         case 'm':
             use_mmap = 1;
@@ -113,6 +174,25 @@ char *parse_args(int *argc, char ***argv)
         usage(1);
     }
 
+    if (nrange > 0) {
+        // Sanitize range list
+        qsort(range_list, nrange, sz, &range_sort);
+        for (i=1; i < nrange; ) {
+            if (range_list[i].start <= range_list[i-1].end+1) {
+                if (range_list[i].end > range_list[i-1].end)
+                    range_list[i-1].end = range_list[i].end;
+                memcpy(range_list+i, range_list+i+1, (nrange-i) * sz);
+                nrange--;
+            } else
+                i++;
+        }
+
+        // Shrink array
+        range_tmp = malloc(nrange * sz);
+        memcpy(range_tmp, range_list, nrange * sz);
+        free(range_list);
+        range_list = range_tmp;
+    }
 
     return file;
 }
@@ -121,7 +201,7 @@ char *parse_args(int *argc, char ***argv)
 int main(int argc, char **argv)
 {
     char *file = NULL;
-    int i, block, err, found;
+    int i, n, block, err, found, print_block, range_start;
     sdf_file_t *h;
     sdf_block_t *b;
     comm_t comm;
@@ -161,28 +241,45 @@ int main(int argc, char **argv)
     sdf_read_summary(h);
 
     found = 0;
+    range_start = 0;
+    print_block = 0;
     if (metadata) {
-        if (variable_ids)
+        if (variable_ids || nrange) {
             DBG_FLUSH();
-        else
+            print_block = 1;
+        } else
             DBG_PRINT_FLUSH();
         found = 1;
     }
 
     // Construct the metadata blocklist using the contents of the buffer
     for (i = 0; i < h->nblocks; i++) {
+        if (print_block) SDF_PRNT("\n  Block number: %i\n", i+1);
         sdf_read_block_info(h);
 
-        if (variable_ids && metadata) {
+        if (print_block) {
             found = 0;
-            last_id = variable_ids;
-            while (last_id) {
-                if (!memcmp(h->current_block->id, last_id->id,
-                        strlen(last_id->id)+1)) {
+
+            for (n = range_start; n < nrange; n++) {
+                if ((i+1) < range_list[n].start)
+                    break;
+                if ((i+1) <= range_list[n].end) {
                     found = 1;
                     break;
                 }
-                last_id = last_id->next;
+                range_start++;
+            }
+
+            if (!found && variable_ids) {
+                last_id = variable_ids;
+                while (last_id) {
+                    if (!memcmp(h->current_block->id, last_id->id,
+                            strlen(last_id->id)+1)) {
+                        found = 1;
+                        break;
+                    }
+                    last_id = last_id->next;
+                }
             }
         }
 
@@ -191,6 +288,8 @@ int main(int argc, char **argv)
         else
             DBG_FLUSH();
     }
+
+    if (print_block) DBG_FLUSH();
 
     free(h->buffer);
     h->buffer = NULL;
@@ -205,23 +304,45 @@ int main(int argc, char **argv)
     if (!contents) return 0;
 
     found = 1;
+    range_start = 0;
+    if (variable_ids || nrange) print_block = 1;
+
     b = h->current_block = h->blocklist;
 
     for (i=0; i<h->nblocks; i++) {
         b = h->current_block;
-        if (variable_ids) {
+        if (print_block) {
             found = 0;
-            h->print = 0;
-            last_id = variable_ids;
-            while (last_id) {
-                if (!memcmp(b->id, last_id->id, strlen(last_id->id)+1)) {
-                    h->print = 1;
+
+            for (n = range_start; n < nrange; n++) {
+                if ((i+1) < range_list[n].start)
+                    break;
+                if ((i+1) <= range_list[n].end) {
                     found = 1;
-                    sdf_read_data(h);
-                     
                     break;
                 }
-                last_id = last_id->next;
+                range_start++;
+            }
+
+            if (!found && variable_ids) {
+                last_id = variable_ids;
+                while (last_id) {
+                    if (!memcmp(h->current_block->id, last_id->id,
+                            strlen(last_id->id)+1)) {
+                        found = 1;
+                        break;
+                    }
+                    last_id = last_id->next;
+                }
+            }
+
+            if (found) {
+                h->print = 1;
+                sdf_read_data(h);
+#ifdef SDF_DEBUG
+                DBG_PRINT_FLUSH();
+#endif
+                h->print = 0;
             }
         } else
             sdf_read_data(h);
