Warwick-Plasma/epoch

commit 761e415cd011d0590960feb384c0dd25e8d232cb

Fixed field setup when restarting.

epoch1d/src/fields.f90
@@ -333,4 +333,25 @@ CONTAINS
 
   END SUBROUTINE update_eb_fields_final
 
+
+
+  SUBROUTINE update_eb_restart
+
+    hdt  = 0.5_num * dt
+    hdtx = hdt / dx
+    hdty = hdt / dy
+
+    cnx = hdtx * c**2
+    cny = hdty * c**2
+
+    fac = hdt / epsilon0
+
+    CALL update_b_field
+
+    CALL bfield_final_bcs
+
+    CALL efield_bcs
+
+  END SUBROUTINE update_eb_restart
+
 END MODULE fields

epoch3d/src/fields.f90
@@ -558,4 +558,25 @@ CONTAINS
 
   END SUBROUTINE update_eb_fields_final
 
+
+
+  SUBROUTINE update_eb_restart
+
+    hdt  = 0.5_num * dt
+    hdtx = hdt / dx
+    hdty = hdt / dy
+
+    cnx = hdtx * c**2
+    cny = hdty * c**2
+
+    fac = hdt / epsilon0
+
+    CALL update_b_field
+
+    CALL bfield_final_bcs
+
+    CALL efield_bcs
+
+  END SUBROUTINE update_eb_restart
+
 END MODULE fields

epoch2d/src/fields.f90
@@ -451,4 +451,25 @@ CONTAINS
 
   END SUBROUTINE update_eb_fields_final
 
+
+
+  SUBROUTINE update_eb_restart
+
+    hdt  = 0.5_num * dt
+    hdtx = hdt / dx
+    hdty = hdt / dy
+
+    cnx = hdtx * c**2
+    cny = hdty * c**2
+
+    fac = hdt / epsilon0
+
+    CALL update_b_field
+
+    CALL bfield_final_bcs
+
+    CALL efield_bcs
+
+  END SUBROUTINE update_eb_restart
+
 END MODULE fields

epoch1d/src/epoch1d.F90
@@ -43,7 +43,7 @@ PROGRAM pic
   CHARACTER(LEN=64) :: deck_file = 'input.deck'
   CHARACTER(LEN=*), PARAMETER :: data_dir_file = 'USE_DATA_DIRECTORY'
   CHARACTER(LEN=64) :: timestring
-  REAL(num) :: runtime
+  REAL(num) :: runtime, dt0
 
   step = 0
   time = 0.0_num
@@ -111,14 +111,19 @@ PROGRAM pic
 
   CALL particle_bcs
   CALL efield_bcs
+
   IF (ic_from_restart) THEN
+    dt0 = dt
+    IF (dt_from_restart .GT. 0) dt0 = dt_from_restart
+    time = time - dt0 / 2.0_num
     CALL bfield_bcs(.TRUE.)
-    CALL update_eb_fields_final
+    CALL update_eb_restart
     CALL moving_window
   ELSE
     CALL bfield_final_bcs
-    time = time + dt / 2.0_num
   ENDIF
+
+  time = time + dt / 2.0_num
 
   ! Setup particle migration between species
   IF (use_particle_migration) CALL initialise_migration

epoch2d/src/epoch2d.F90
@@ -43,7 +43,7 @@ PROGRAM pic
   CHARACTER(LEN=64) :: deck_file = 'input.deck'
   CHARACTER(LEN=*), PARAMETER :: data_dir_file = 'USE_DATA_DIRECTORY'
   CHARACTER(LEN=64) :: timestring
-  REAL(num) :: runtime
+  REAL(num) :: runtime, dt0
 
   step = 0
   time = 0.0_num
@@ -111,14 +111,19 @@ PROGRAM pic
 
   CALL particle_bcs
   CALL efield_bcs
+
   IF (ic_from_restart) THEN
+    dt0 = dt
+    IF (dt_from_restart .GT. 0) dt0 = dt_from_restart
+    time = time - dt0 / 2.0_num
     CALL bfield_bcs(.TRUE.)
-    CALL update_eb_fields_final
+    CALL update_eb_restart
     CALL moving_window
   ELSE
     CALL bfield_final_bcs
-    time = time + dt / 2.0_num
   ENDIF
+
+  time = time + dt / 2.0_num
 
   ! Setup particle migration between species
   IF (use_particle_migration) CALL initialise_migration

epoch3d/src/epoch3d.F90
@@ -43,7 +43,7 @@ PROGRAM pic
   CHARACTER(LEN=64) :: deck_file = 'input.deck'
   CHARACTER(LEN=*), PARAMETER :: data_dir_file = 'USE_DATA_DIRECTORY'
   CHARACTER(LEN=64) :: timestring
-  REAL(num) :: runtime
+  REAL(num) :: runtime, dt0
 
   step = 0
   time = 0.0_num
@@ -111,14 +111,19 @@ PROGRAM pic
 
   CALL particle_bcs
   CALL efield_bcs
+
   IF (ic_from_restart) THEN
+    dt0 = dt
+    IF (dt_from_restart .GT. 0) dt0 = dt_from_restart
+    time = time - dt0 / 2.0_num
     CALL bfield_bcs(.TRUE.)
-    CALL update_eb_fields_final
+    CALL update_eb_restart
     CALL moving_window
   ELSE
     CALL bfield_final_bcs
-    time = time + dt / 2.0_num
   ENDIF
+
+  time = time + dt / 2.0_num
 
   ! Setup particle migration between species
   IF (use_particle_migration) CALL initialise_migration

commit 60bb0d07a71a4c4dad45e6190b4e33bd6f4d5a57

Fixed typos introduced by commit 761e415c
This fixes issue #1259

epoch1d/src/fields.f90
@@ -339,10 +339,8 @@ CONTAINS
 
     hdt  = 0.5_num * dt
     hdtx = hdt / dx
-    hdty = hdt / dy
 
     cnx = hdtx * c**2
-    cny = hdty * c**2
 
     fac = hdt / epsilon0
 

epoch3d/src/fields.f90
@@ -565,9 +565,11 @@ CONTAINS
     hdt  = 0.5_num * dt
     hdtx = hdt / dx
     hdty = hdt / dy
+    hdtz = hdt / dz
 
     cnx = hdtx * c**2
     cny = hdty * c**2
+    cnz = hdtz * c**2
 
     fac = hdt / epsilon0
 