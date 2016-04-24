1eddcd542403cb142e932b025b84468f3794c0d1
Stephan Kuschel <stephan.kuschel@uni-jena.de>
2016-04-24 15:36:05 +0200

    replace test-all.sh by run-tests-epoch.sh
    also .gitlab-ci.yml is changed accordingly.
    
    run-tests-epoch.sh is running the tests of epoch{1,2,3}d after another.
    SDF is build only as needed for the tests. Testing the SDF library
    should be part of the test suite of the libarary istself.

---
+++ run-tests-epoch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright (C) 2016 Stephan Kuschel <Stephan.Kuschel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# THIS FILE MUST RUN WITHOUT ERROR ON EVERY COMMIT!
+
+set -e
+
+# Build SDF/C and install python sdf reader
+(cd SDF/C; make)
+SDF/utilities/build
+
+# run the actual tests
+epoch1d/run-tests.py
