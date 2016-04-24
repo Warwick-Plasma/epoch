23bbd564d6e4879d19720c49eeb505a6e4e7a3d7
Stephan Kuschel <stephan.kuschel@uni-jena.de>
2016-04-24 20:14:42 +0200

    test suite for epoch2d and epoch3d
    including the laser test example only

---
+++ epoch3d/run-tests.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2016 Stephan Kuschel <Stephan.Kuschel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import nose
+import subprocess
+import os
+
+_curfiledir = os.path.dirname(os.path.abspath(__file__))
+
+def setcwd(relative=None):
+    '''
+    resets the current working directiory to the path
+    of this file.
+    '''
+    os.chdir(_curfiledir)
+    if relative:
+        os.chdir(relative)
+
+def compileepoch():
+    '''
+    compiles the EPOCH code. The exit code of 'make' is returned.
+    '''
+    setcwd()
+    exitcode = subprocess.call('make', shell=True)
+    if exitcode != 0:
+        print('compiling EPOCH errored (exitcode {})'.format(exitcode))
+    return exitcode
+
+
+def run_tests(args):
+    '''
+    use nose to collect the tests and run them all.
+    '''
+    noseargv = ['']
+    if args.test:
+        noseargv += ['tests.test_' + args.test]
+    setcwd()
+    testsok = nose.run(argv=noseargv)
+    return testsok
+
+
+def clean():
+    '''
+    clean the tests directory and all its subdirectoryies
+    by calling 'make clean' in each of them.
+    '''
+    setcwd()
+    subprocess.call('rm -rf tests/__pycache__', shell=True)  # python3
+    subprocess.call('rm -rf tests/*.pyc', shell=True)  # python2
+    setcwd()
+    files = [os.path.join('tests', f) for f in os.listdir('tests')]
+    dirs = [d for d in files if os.path.isdir(d)]
+    for d in dirs:
+        # call 'make clean' in every subdir
+        setcwd(d)
+        subprocess.call('make clean', shell=True)
+
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='''
+    This runs the testsuite for EPOCH1D.
+    It compiles EPOCH and runs the tests.
+    It does NOT: install the python SDF reader or any other dependencies,
+    which might be needed!
+    ''')
+    parser.add_argument('test', nargs='?', help='''
+    run only a single test specified by its name, i.e. 'laser'
+    ''')
+    parser.add_argument('--clean', '-c', action='store_true', help=clean.__doc__)
+    parser.add_argument('--build', '-b', action='store_true', help='''
+    build only. Do not run the code.
+    ''')
+    args = parser.parse_args()
+
+    if args.clean:
+        clean()
+        exit()
+    epochexitcode = compileepoch()
+    if epochexitcode != 0:
+        exit(epochexitcode)
+    if args.build:
+        exit()
+    testsok = run_tests(args)
+    exit(int(not testsok))
+
+
+if __name__=='__main__':
+    main()
---
+++ epoch3d/tests/test_laser.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2016 Stephan Kuschel <Stephan.Kuschel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import sdf
+import matplotlib; matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+import unittest
+from . import SimTest
+
+
+def showdatafields(sdffile):
+    data = sdf.read(sdffile, dict=True)
+    print('Data fields present in "' + sdffile + '":')
+    print(data.keys())
+
+def plotdump(sdffile, key, ax):
+    data = sdf.read(sdffile, dict=True)
+    array = data[key].data[:,80,:]
+    ax.imshow(array.T, origin='lower')
+    ax.set_title('{:2.1f} fs'.format(data['Header']['time']*1e15))
+
+def plotevolution(key):
+    sdffiles = ['{:04d}.sdf'.format(i) for i in range(8)]
+    fig, axarr = plt.subplots(2,4, figsize=(16,9))
+    for (sdffile, ax) in zip(sdffiles, np.ravel(axarr)):
+        plotdump(sdffile, key, ax)
+    fig.suptitle(key)
+    fig.savefig(key.replace('/','_') + '.png', dpi=160)
+
+def createplots():
+    #showdatafields('0000.sdf')
+    plotevolution('Electric Field/Ex')
+
+
+class test_laser(SimTest):
+
+    def totaleyassert(self, dump, val, digits=4):
+        data = sdf.read(dump, dict=True)
+        ey = data['Electric Field/Ex'].data**2
+        self.assertAlmostEqual(np.sum(ey), val, digits)
+
+    def test_createplots(self):
+        createplots()
+
+    def test_Eex0000(self):
+        self.totaleyassert('0000.sdf', 0.0)
+
+    def test_Eex0001(self):
+        self.totaleyassert('0001.sdf', 9.655775255668219e+24)
+
+    def test_Eex0003 (self):
+        self.totaleyassert('0003.sdf', 2.8181721274619696e+25)
+
+    def test_Eex0007(self):
+        self.totaleyassert('0007.sdf', 6.5331948154961507e+25)
+
+if __name__=='__main__':
+    unittest.main()
+
+
