55ca257d8a1f1d25de87f0522aa694db5bde5556
Stephan Kuschel <stephan.kuschel@uni-jena.de>
2016-04-24 13:04:46 +0200

    better logic in run-tests.py

--- epoch1d/run-tests.py
+++ epoch1d/run-tests.py
@@ -9,23 +9,21 @@ import os
 def setcwd():
     os.chdir(os.path.dirname(__file__))
 
-def compileepoch(exitonerror=True):
+def compileepoch():
     setcwd()
     exitcode = subprocess.call('make', shell=True)
     if exitcode != 0:
         print('compiling EPOCH errored (exitcode {})'.format(exitcode))
-        if exitonerror:
-            exit(exitcode)
     return exitcode
 
 
 def run_tests(args):
-    compileepoch()
     noseargv = ['']
     if args.test:
         noseargv += ['tests.test_' + args.test]
     setcwd()
-    nose.run(argv=noseargv)
+    testsok = nose.run(argv=noseargv)
+    return testsok
 
 
 def main():
@@ -34,8 +32,11 @@ def main():
     parser.add_argument('test', nargs='?')
     args = parser.parse_args()
 
-    run_tests(args)
-
+    epochexitcode = compileepoch()
+    if epochexitcode != 0:
+        exit(epochexitcode)
+    testsok = run_tests(args)
+    exit(int(not testsok))
 
 
 if __name__=='__main__':
