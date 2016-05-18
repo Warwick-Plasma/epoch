34229d0afcda842002681abb6750d414ddedca6b
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2016-05-18 14:20:21 +0100

    Implemented improved calculation of kinetic energy in distribution function output.

--- epoch1d/src/io/dist_fn.F90
+++ epoch1d/src/io/dist_fn.F90
@@ -148,7 +148,7 @@ CONTAINS
     REAL(num), DIMENSION(2,c_df_maxdims) :: ranges
     INTEGER, DIMENSION(c_df_maxdims) :: resolution
     INTEGER, DIMENSION(c_df_maxdims) :: cell
-    REAL(num) :: part_weight, part_mc, part_mc2, gamma_m1, start
+    REAL(num) :: part_weight, part_mc, part_mc2, part_p_mc_2, gamma_m1, start
     REAL(num) :: xy_max, yz_max, zx_max
     REAL(num), PARAMETER :: pi2 = 2.0_num * pi
 
@@ -276,7 +276,8 @@ CONTAINS
         part_mc  = current%mass * c
         part_mc2 = part_mc * c
 #endif
-        gamma_m1 = SQRT(SUM((current%part_p / part_mc)**2) + 1.0_num) - 1.0_num
+        part_p_mc_2 = SUM((current%part_p / part_mc)**2)
+        gamma_m1 = part_p_mc_2/(SQRT(part_p_mc_2 + 1.0_num) + 1.0_num)
         px = current%part_p(1)
         py = current%part_p(2)
         pz = current%part_p(3)
@@ -396,7 +397,8 @@ CONTAINS
 #ifndef PER_SPECIES_WEIGHT
       part_weight = current%weight
 #endif
-      gamma_m1 = SQRT(SUM((current%part_p / part_mc)**2) + 1.0_num) - 1.0_num
+      part_p_mc_2 = SUM((current%part_p / part_mc)**2)
+      gamma_m1 = part_p_mc_2/(SQRT(part_p_mc_2 + 1.0_num) + 1.0_num)
       px = current%part_p(1)
       py = current%part_p(2)
       pz = current%part_p(3)
