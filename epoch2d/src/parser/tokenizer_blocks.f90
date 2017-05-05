984b0cae86847fff739a9eb093afb3abd06aa743
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-05-05 14:33:45 +0200

    renamed lehe to lehe_x

--- epoch2d/src/parser/tokenizer_blocks.f90
+++ epoch2d/src/parser/tokenizer_blocks.f90
@@ -206,7 +206,7 @@ CONTAINS
     IF (str_cmp(name, 'yee')) as_constant = c_const_maxwell_solver_yee
     IF (str_cmp(name, 'cowan')) as_constant = c_const_maxwell_solver_cowan
     IF (str_cmp(name, 'pukhov')) as_constant = c_const_maxwell_solver_pukhov
-    IF (str_cmp(name, 'lehe')) as_constant = c_const_maxwell_solver_lehe
+    IF (str_cmp(name, 'lehe_x')) as_constant = c_const_maxwell_solver_lehe
 
     ! User submitted constant using 'Register'
     DO i = 1, n_new_constant
