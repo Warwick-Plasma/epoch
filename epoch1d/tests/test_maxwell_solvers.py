880e70eba731d399de18b8b5e37ce0a1615115dc
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-05-08 16:25:50 +0200

    Improved EPOCH1D Lehe solver test case

--- epoch1d/tests/test_maxwell_solvers.py
+++ epoch1d/tests/test_maxwell_solvers.py
@@ -20,6 +20,7 @@ import sdf
 import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
+import os.path as osp
 import unittest
 import platform
 from . import SimTest
@@ -31,20 +32,24 @@ def showdatafields(sdffile):
     print(data.keys())
 
 def plotdump(sdffile, key, ax):
-    data = sdf.read(sdffile, dict=True)
-    axis = data[key].grid_mid.data[0]*1e6
-    array = data[key].data
-    ax.plot(axis, array)
-    ax.set_title('{:2.1f} fs'.format(data['Header']['time']*1e15))
+    data_yee = sdf.read(sdffile[0], dict=True)
+    data_lehe = sdf.read(sdffile[1], dict=True)
+    axis = data_yee[key].grid_mid.data[0]*1e6
+    array_yee = data_yee[key].data
+    array_lehe = data_lehe[key].data
+    ax.plot(axis, array_yee, label='Yee', linewidth=1)
+    ax.plot(axis, array_lehe, label='Lehe', linewidth=1)
+    ax.set_title('{:2.1f} fs'.format(data_yee['Header']['time']*1e15))
     ax.set_xlabel('x [µm]')
+    ax.legend()
 
 def plotevolution(key):
-    sdffiles = ['{:04d}.sdf'.format(i) for i in range(8)]
+    sdffiles = zip([osp.join('yee','{:04d}.sdf'.format(i)) for i in range(8)],   [osp.join('lehe_x','{:04d}.sdf'.format(i)) for i in range(8)])
     fig, axarr = plt.subplots(2,4, figsize=(16,9))
     for (sdffile, ax) in zip(sdffiles, np.ravel(axarr)):
         plotdump(sdffile, key, ax)
     fig.suptitle(key)
-    fig.savefig(key.replace('/','_') + '.png', dpi=160)
+    fig.savefig(key.replace('/','_') + '.png', dpi=320)
 
 def createplots():
     if platform.system() == 'Darwin':
@@ -57,12 +62,7 @@ def createplots():
 class test_maxwell_solvers(SimTest):
 
     def test_createplots(self):
-        os.chdir('yee')
         createplots()
-
-        os.chdir('../lehe_x')
-        createplots()
-
 
 if __name__=='__main__':
     unittest.main()
