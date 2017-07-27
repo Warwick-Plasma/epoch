03722be6dd6ea1d8e0fd621b0fc7dd4f87e4dcdc
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-07-27 11:34:44 +0200

    updated epoch2d custom_stencils test

--- epoch2d/tests/test_custom_stencils.py
+++ epoch2d/tests/test_custom_stencils.py
@@ -16,15 +16,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
-import sdf
-import matplotlib; matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+import types
 import os
 import os.path as osp
 import unittest
 import platform
+
+import numpy as np
+import matplotlib; matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sdf
+
 from . import SimTest
+
 
 micron = 1e-6
 femto = 1e-15
@@ -40,6 +44,8 @@ x_max = -x_min
 y_min = x_min
 y_max = x_max
 
+dt_multiplier = 0.99
+
 lambda_l = 0.5 * micron
 x0 = -12.0 * micron # m
 t0 = 8 * femto # s
@@ -47,110 +53,132 @@ t0 = 8 * femto # s
 # derived quantities from the above
 k_l = 2*np.pi/lambda_l
 dx = (x_max-x_min)/nx
-kappax = k_l * dx
-print('kappax: ', kappax)
 dy = (y_max-y_min)/ny
 
-#dt_lehe   = 0.95 * 1.0/np.sqrt(max(1.0/dx**2, 1.0/dy**2)) / c
-dt_yee    = 0.99 * dx * dy / np.sqrt(dx**2+dy**2) / c
+dt_lehe   = dt_multiplier * 1.0/np.sqrt(max(1.0/dx**2, 1.0/dy**2)) / c
+dt_yee    = dt_multiplier * dx * dy / np.sqrt(dx**2+dy**2) / c
+dt_pukhov = dt_multiplier * min(dx,dy)/c
 
+dt = dict()
+dt['optimized'] = 0.9082126568805592 * dx / c
+dt['optimized_symm'] = 0.8988685682513151 * dx / c
+dt['optimized_xaxis'] = 0.956632159129662 * dx / c
 
+vg = dict()
+# calculate_omega.py --Y 3 --dt-multiplier 0.99 --params  0.9082126568805592
+# 0.04075757835916255 -0.04142032920970152 -0.20827814817872584
+# --physical-dx 1e-7 --laser-wavelength 5e-7
+vg['optimized'] = 1.0490493627815458 * c
 
-def showdatafields(sdffile):
-    data = sdf.read(sdffile, dict=True)
-    print('Data fields present in "' + sdffile + '":')
-    print(data.keys())
+# calculate_omega.py --Y 3 --dt-multiplier 0.99 --symmetric --params
+# 0.8988685682513151 0.01862292597327679 -0.04155873453935287 --physical-dx
+# 1e-7 --laser-wavelength 5e-7
+vg['optimized_symm'] = 1.044753207834214 * c
 
-def plotdump(sdffile, key, ax):
-    data = sdf.read(sdffile, dict=True)
-    array = data[key].data
-    xaxis = data[key].grid_mid.data[0]*1e6
-    yaxis = data[key].grid_mid.data[1]*1e6
-    im = ax.imshow(array.T, extent=[min(xaxis), max(xaxis), min(yaxis), max(yaxis)])
-    im.set_clim([-1e11,1e11])
-    t = data['Header']['time']
-    dx = np.asscalar(data[key].grid_mid.data[0][1] - data[key].grid_mid.data[0][0])
+# calculate_omega.py --Y 3 --dt-multiplier 0.99 --params 0.956632159129662
+# 0.025096871992206993 -0.017744324957063393 -0.0009692545471922645
+# --physical-dx 1e-7 --laser-wavelength 5e-7
+vg['optimized_xaxis'] = 1.0197513694119302 * c
 
-    #vg_lehe = c*(1.0 + 2.0*(1.0-c*dt_lehe/dx)*(k_l*dx/2.0)**2)
-    #vg_yee  = c*(1.0 - (1.0-c*dt/dx)*(k_l*dx/2.0)**2)
-    vg_yee = c*np.cos(k_l*dx/2.0)/np.sqrt(1-(c*dt_yee/dx*np.sin(k_l*dx/2.0))**2)
+vg_lehe   = c*(1.0 + 2.0*(1.0-c*dt_lehe/dx)*(k_l*dx/2.0)**2)
+vg_yee    = c*np.cos(k_l*dx/2.0)/np.sqrt(1-(c*dt_yee/dx*np.sin(k_l*dx/2.0))**2)
+vg_pukhov = c*np.cos(k_l*dx/2.0)/np.sqrt(1-(c*dt_pukhov/dx*np.sin(k_l*dx/2.0))**2)
 
-    # calculate_omega.py --dt-multiplier 0.99 --Y 3 --params 0.9082126568805592 0.04075757835916255
-    # -0.04142032920970152 -0.20827814817872584 --physical-dx 1e-7 --laser-wavelength 5e-7
-    # yields vg=1.0490493627815458 c
-    vg_opt = 1.0490493627815458 * c
+def xt2(sdffile, key = 'Electric Field/Ey'):
+    t = sdffile['Header']['time']
+    xaxis = sdffile[key].grid_mid.data[0]
+    data = sdffile[key].data
+    b = np.sum(data**2)
+    if b>0 and t>0:
+        x = np.sum(xaxis[:,np.newaxis]*data**2)/b
+    else:
+        x = None
 
-    # calculate_omega.py --dt-multiplier 0.99 --Y 3 --symmetric  --params 0.8988685682513151
-    # 0.01862292597327679 -0.04155873453935287 --physical-dx 1e-7 --laser-wavelength 5e-7
-    # yields vg=1.044753207834214 c
-    vg_opt_symm = 1.044753207834214 * c
-
-    # calculate_omega.py --dt-multiplier 0.99 --Y 3  --params 0.956632159129662  0.025096871992206993
-    # -0.017744324957063393 -0.0009692545471922645 --physical-dx 1e-7 --laser-wavelength 5e-7
-    # yields vg=1.0197513694119302 c
-    vg_opt_xaxis = 1.0197513694119302 * c
-
-
-    x = (x0 + c*(t-t0))*1e6
-    x_opt = (x0 + vg_opt*(t-t0))*1e6
-    x_opt_xaxis = (x0 + vg_opt_xaxis*(t-t0))*1e6
-    x_yee = (x0 + vg_yee*(t-t0))*1e6
-
-    if x > 0:
-        ax.plot([x, x], [min(yaxis), 0.9*min(yaxis)], color='k', linestyle='-', linewidth=2, alpha=0.25)
-        ax.plot([x, x], [0.9*max(yaxis), max(yaxis)], color='k', linestyle='-', linewidth=2, alpha=0.25)
-
-        ax.plot([x_opt, x_opt], [min(yaxis), 0.9*min(yaxis)], color='b', linestyle='-', linewidth=2, alpha=0.25)
-        ax.plot([x_opt, x_opt], [0.9*max(yaxis), max(yaxis)], color='b', linestyle='-', linewidth=2, alpha=0.25)
-
-        ax.plot([x_opt_xaxis, x_opt_xaxis], [min(yaxis), 0.9*min(yaxis)], color='y', linestyle='-', linewidth=2, alpha=0.25)
-        ax.plot([x_opt_xaxis, x_opt_xaxis], [0.9*max(yaxis), max(yaxis)], color='y', linestyle='-', linewidth=2, alpha=0.25)
-
-        ax.plot([x_yee, x_yee], [min(yaxis), 0.9*min(yaxis)], color='r', linestyle='-', linewidth=2, alpha=0.25)
-        ax.plot([x_yee, x_yee], [0.9*max(yaxis), max(yaxis)], color='r', linestyle='-', linewidth=2, alpha=0.25)
-
-    ax.set_title('{:2.1f} fs'.format(data['Header']['time']*1e15))
-
-def plotevolution(path, key):
-    sdffiles = [osp.join(path, '{:04d}.sdf'.format(i)) for i in range(3)]
-
-    fig, axarr = plt.subplots(1,3, figsize=(10,4))
-    axarr[0].set_ylabel('y [µm]')
-    for (sdffile, ax) in zip(sdffiles, np.ravel(axarr)):
-
-        plotdump(sdffile, key, ax)
-        ax.set_xlabel('x [µm]')
-
-    if path.endswith('optimized'):
-        title = 'Opt'
-    elif path.endswith('optimized_symm'):
-        title = 'Opt Symm'
-    elif path.endswith('optimized_xaxis'):
-        title = 'Opt xaxis'
-
-    fig.suptitle(key)
-
-    fig.tight_layout()
-    fig.savefig(key.replace('/','_') + '_' + title + '.png', dpi=320)
-
-def createplots(path):
-    plotevolution(path, 'Electric Field/Ey')
+    return t, x
 
 
 class test_custom_stencils(SimTest):
-    def __init__(self, arg):
-        super(test_custom_stencils, self).__init__(arg)
+    solvers = ['optimized','optimized_symm','optimized_xaxis']
+
+    @classmethod
+    def setUpClass(cls):
+        super(test_custom_stencils, cls).setUpClass()
+
+        dumps = cls.dumps = {}
+        for solver in cls.solvers:
+            l = dumps.setdefault(solver, [])
+            for dump in [osp.join(solver, '{:04d}.sdf'.format(i)) for i in range(4)]:
+                l.append(sdf.read(dump, dict=True))
+
+    def test_createplot(self):
         if platform.system() == 'Darwin':
             print('macosx backend')
             plt.switch_backend('macosx')
 
-    def test_createplots(self):
-        base = os.getcwd()
+        for solver in self.solvers:
+            key = 'Electric Field/Ey'
+            fig, axarr = plt.subplots(1,3, figsize=(10,4))
+            axarr[0].set_ylabel(r'y [${\mu}\mathrm{m}$]')
+            dumps = self.dumps[solver]
+            dumps = dumps[1:4]
+            for (dump, ax) in zip(dumps, np.ravel(axarr)):
+                array = dump[key].data
+                xaxis = dump[key].grid_mid.data[0]*1e6
+                yaxis = dump[key].grid_mid.data[1]*1e6
+                im = ax.imshow(array.T, extent=[min(xaxis), max(xaxis), min(yaxis), max(yaxis)])
+                im.set_clim([-1e11,1e11])
+                t = dump['Header']['time']
+                dx = np.asscalar(dump[key].grid_mid.data[0][1] - dump[key].grid_mid.data[0][0])
 
-        createplots(osp.join(base, 'optimized'))
-        createplots(osp.join(base, 'optimized_symm'))
-        createplots(osp.join(base, 'optimized_xaxis'))
+                x = (x0 + c*(t-t0))*1e6
+                x_opt = (x0 + vg['optimized']*(t-t0))*1e6
+                x_opt_xaxis = (x0 + vg['optimized_xaxis']*(t-t0))*1e6
+                x_yee = (x0 + vg_yee*(t-t0))*1e6
 
+                if x > 0:
+                    ax.plot([x, x], [min(yaxis), 0.9*min(yaxis)], color='k', linestyle='-', linewidth=2, alpha=0.25)
+                    ax.plot([x, x], [0.9*max(yaxis), max(yaxis)], color='k', linestyle='-', linewidth=2, alpha=0.25)
+
+                    ax.plot([x_opt, x_opt], [min(yaxis), 0.9*min(yaxis)], color='b', linestyle='-', linewidth=2, alpha=0.25)
+                    ax.plot([x_opt, x_opt], [0.9*max(yaxis), max(yaxis)], color='b', linestyle='-', linewidth=2, alpha=0.25)
+
+                    ax.plot([x_opt_xaxis, x_opt_xaxis], [min(yaxis), 0.9*min(yaxis)], color='y', linestyle='-', linewidth=2, alpha=0.25)
+                    ax.plot([x_opt_xaxis, x_opt_xaxis], [0.9*max(yaxis), max(yaxis)], color='y', linestyle='-', linewidth=2, alpha=0.25)
+
+                    ax.plot([x_yee, x_yee], [min(yaxis), 0.9*min(yaxis)], color='r', linestyle='-', linewidth=2, alpha=0.25)
+                    ax.plot([x_yee, x_yee], [0.9*max(yaxis), max(yaxis)], color='r', linestyle='-', linewidth=2, alpha=0.25)
+
+                ax.set_title('{:2.1f} fs'.format(t*1e15))
+
+            if solver == 'optimized':
+                title = 'Opt'
+            elif solver == 'optimized_symm':
+                title = 'Opt Symm'
+            elif solver == 'optimized_xaxis':
+                title = 'Opt xaxis'
+
+            fig.suptitle(key+', c*dt/dx={:0.03f}'.format(c * dt[solver] / dx))
+
+            fig.tight_layout()
+            fig.savefig(key.replace('/','_') + '_' + title + '.png', dpi=320)
+
+
+    def test_group_velocity(self):
+        tx = {}
+        for solver in self.solvers:
+            tx[solver] = np.array([xt2(dump) for dump in self.dumps[solver][1:]])
+        print(tx)
+
+        for solver, data in tx.items():
+            vg_sim = np.polyfit(data[:,0], data[:,1], 1)[0]
+
+            # For reference, right here, right now the following line prints
+            # optimized 314212524.201 314497087.03161335 0.00090481865174
+            # optimized_symm 312571859.841 313209132.1800039 0.00203465440037
+            # optimized_xaxis 305472528.152 305713769.58486855 0.000789108824096
+            print(solver, vg_sim, vg[solver], abs(vg_sim-vg[solver])/vg[solver])
+
+            assert np.isclose(vg_sim, vg[solver], rtol=0.01) #
 
 
 if __name__=='__main__':
