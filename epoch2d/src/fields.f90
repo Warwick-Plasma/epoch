Warwick-Plasma/epoch

commit 56d0964a3ffc180d60d4d4e5d3c1faf6f0afd5b6

Added informational output of stencil parameters

epoch2d/src/fields.f90
@@ -79,6 +79,17 @@ CONTAINS
       alphay = 1.0_num - 2.0_num * betayx
     ENDIF
 
+    IF ((rank == 0) .AND. (maxwell_solver .NE. c_maxwell_solver_yee)) THEN
+        PRINT*,''
+        PRINT*,'Maxwell solver set to the following parameters:'
+        PRINT*,'alpha=', alphax, alphay
+        PRINT*,' beta=', betaxy, betayx
+        PRINT*,'gamma=', gammax, gammay
+        PRINT*,'delta=', deltax, deltay
+        PRINT*,'c*dt/dx=', dt * c / dx
+        PRINT*,''
+    ENDIF
+
   END SUBROUTINE set_maxwell_solver
 
 

epoch1d/src/fields.f90
@@ -62,6 +62,15 @@ CONTAINS
       alphax = 1.0_num - 3.0_num * deltax
     ENDIF
 
+    IF ((rank == 0) .AND. (maxwell_solver .NE. c_maxwell_solver_yee)) THEN
+        PRINT*,''
+        PRINT*,'Maxwell solver set to the following parameters:'
+        PRINT*,'alphax=', alphax
+        PRINT*,'deltax=', deltax
+        PRINT*,'c*dt/dx=', dt * c / dx
+        PRINT*,''
+    ENDIF
+
   END SUBROUTINE set_maxwell_solver
 
 

epoch3d/src/fields.f90
@@ -132,6 +132,19 @@ CONTAINS
       alphaz = 1.0_num - 2.0_num * betazx - 2.0_num * betazy
     ENDIF
 
+    IF ((rank == 0) .AND. (maxwell_solver .NE. c_maxwell_solver_yee)) THEN
+        PRINT*,''
+        PRINT*,'Maxwell solver set to the following parameters:'
+        PRINT*,'alpha=', alphax, alphay, alphaz
+        PRINT*,'betax=', betaxy, betaxz
+        PRINT*,'betay=', betayx, betayz
+        PRINT*,'betaz=', betazx, betazy
+        PRINT*,'gamma=', gammax, gammay, gammaz
+        PRINT*,'delta=', deltax, deltay, deltaz
+        PRINT*,'c*dt/dx=', dt * c / dx
+        PRINT*,''
+    ENDIF
+
   END SUBROUTINE set_maxwell_solver
 
 

commit 80b4cc0e73eb3e0e3a4d50abd202d68ef912652c

fixed bug in epoch2d/src/fields.f90 introduced in 56d0964a

epoch2d/src/fields.f90
@@ -84,7 +84,6 @@ CONTAINS
         PRINT*,'Maxwell solver set to the following parameters:'
         PRINT*,'alpha=', alphax, alphay
         PRINT*,' beta=', betaxy, betayx
-        PRINT*,'gamma=', gammax, gammay
         PRINT*,'delta=', deltax, deltay
         PRINT*,'c*dt/dx=', dt * c / dx
         PRINT*,''