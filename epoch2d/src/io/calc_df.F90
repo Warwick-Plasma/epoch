49ea7c7455be66cf9f77a3fa84b93cdd02649460
Chris Brady <c.s.brady@warwick.ac.uk>
2017-08-14 11:23:09 +0100

    Added injectors to EPOCH2D

--- epoch2d/src/io/calc_df.F90
+++ epoch2d/src/io/calc_df.F90
@@ -30,16 +30,14 @@ CONTAINS
 
     CALL processor_summation_bcs(data_array, ng)
 
-    IF (x_min_boundary .AND. bc_particle(c_bd_x_min) /= c_bc_periodic &
-        .AND. bc_particle(c_bd_x_min) /= c_bc_reflect) THEN
+    IF (x_min_boundary .AND. bc_particle(c_bd_x_min) == c_bc_reflect) THEN
       DO j = 1-ng, ny+ng
       DO i = 1, ng
         data_array(i,j) = data_array(i,j) + data_array(1-i,j)
       ENDDO
       ENDDO
     ENDIF
-    IF (x_max_boundary .AND. bc_particle(c_bd_x_max) /= c_bc_periodic &
-        .AND. bc_particle(c_bd_x_max) /= c_bc_reflect) THEN
+    IF (x_max_boundary .AND. bc_particle(c_bd_x_max) == c_bc_reflect) THEN
       DO j = 1-ng, ny+ng
       DO i = 1, ng
         data_array(nx-i+1,j) = data_array(nx-i+1,j) + data_array(nx+i,j)
@@ -47,16 +45,14 @@ CONTAINS
       ENDDO
     ENDIF
 
-    IF (y_min_boundary .AND. bc_particle(c_bd_y_min) /= c_bc_periodic &
-        .AND. bc_particle(c_bd_y_min) /= c_bc_reflect) THEN
+    IF (y_min_boundary .AND. bc_particle(c_bd_y_min) == c_bc_reflect) THEN
       DO j = 1, ng
       DO i = 1-ng, nx+ng
         data_array(i,j) = data_array(i,j) + data_array(i,1-j)
       ENDDO
       ENDDO
     ENDIF
-    IF (y_max_boundary .AND. bc_particle(c_bd_y_max) /= c_bc_periodic &
-        .AND. bc_particle(c_bd_y_max) /= c_bc_reflect) THEN
+    IF (y_max_boundary .AND. bc_particle(c_bd_y_max) == c_bc_reflect) THEN
       DO j = 1, ng
       DO i = 1-ng, nx+ng
         data_array(i,ny-j+1) = data_array(i,ny-j+1) + data_array(i,ny+j)
