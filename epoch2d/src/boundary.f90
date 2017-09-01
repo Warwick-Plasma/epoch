7ad5ca05e8c7e5fb01cad3c9c73f43e6480ddcd8
Chris Brady <C.S.Brady@warwick.ac.uk>
2017-09-01 17:58:42 +0100

    More fixes for per species periodic boundaries

--- epoch2d/src/boundary.f90
+++ epoch2d/src/boundary.f90
@@ -720,7 +720,10 @@ CONTAINS
     DEALLOCATE(temp)
     CALL MPI_TYPE_FREE(subarray, errcode)
 
-    CALL field_bc(array, ng)
+    array(:0,:) = 0.0_num
+    array(:,:0) = 0.0_num
+    array(nx+1:,:) = 0.0_num
+    array(:,ny+1:) = 0.0_num
 
   END SUBROUTINE processor_summation_bcs
 
