51ef3a4a690013cce28724cd36234f69a66ee7d1
Chris Brady <C.S.Brady@warwick.ac.uk>
2017-11-29 17:41:03 +0000

    Fixed uninitialized variable

--- epoch3d/src/housekeeping/setup.F90
+++ epoch3d/src/housekeeping/setup.F90
@@ -406,6 +406,7 @@ CONTAINS
       species_list(ispecies)%migrate%demotion_energy_factor = 1.0_num
       species_list(ispecies)%migrate%promotion_density = HUGE(1.0_num)
       species_list(ispecies)%migrate%demotion_density = 0.0_num
+      species_list(ispecies)%fill_ghosts = .FALSE.
 #ifndef NO_TRACER_PARTICLES
       species_list(ispecies)%tracer = .FALSE.
 #endif
