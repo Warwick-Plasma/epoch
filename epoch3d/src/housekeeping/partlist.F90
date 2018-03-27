62ecfee0ef4c4f92275c3a21398203e547b26b4d
Alex Arefiev <alexey@austin.utexas.edu>
2018-03-27 21:22:41 +0000

    Update partlist.F90

--- epoch3d/src/housekeeping/partlist.F90
+++ epoch3d/src/housekeeping/partlist.F90
@@ -43,6 +43,10 @@ CONTAINS
   SUBROUTINE setup_partlists
 
     nvar = 3 + c_ndims
+!By O.Jansen, Work_Done
+#ifdef Work_Done_Integrated
+    nvar = nvar + 6
+#endif
 #if !defined(PER_SPECIES_WEIGHT) || defined(PHOTONS)
     nvar = nvar+1
 #endif
@@ -395,6 +399,16 @@ CONTAINS
     array(cpos) = REAL(a_particle%coll_count, num)
     cpos = cpos+1
 #endif
+!By O.Jansen, Work_Done
+#ifdef Work_Done_Integrated
+    array(cpos) = a_particle%work_x
+    array(cpos+1) = a_particle%work_y
+    array(cpos+2) = a_particle%work_z
+    array(cpos+3) = a_particle%work_x_I
+    array(cpos+4) = a_particle%work_y_I
+    array(cpos+5) = a_particle%work_z_I
+    cpos = cpos+6
+#endif
 #ifdef PHOTONS
     array(cpos) = a_particle%optical_depth
     array(cpos+1) = a_particle%particle_energy
@@ -444,6 +458,16 @@ CONTAINS
 #ifdef COLLISIONS_TEST
     a_particle%coll_count = NINT(array(cpos))
     cpos = cpos+1
+#endif
+!By O.Jansen, Work_Done
+#ifdef Work_Done_Integrated
+    a_particle%work_x = array(cpos)
+    a_particle%work_y = array(cpos+1)
+    a_particle%work_z = array(cpos+2)
+    a_particle%work_x_I = array(cpos+3)
+    a_particle%work_y_I = array(cpos+4)
+    a_particle%work_z_I = array(cpos+5)
+    cpos = cpos+6
 #endif
 #ifdef PHOTONS
     a_particle%optical_depth = array(cpos)
