17f4d63a6204c65765f58797ac08dc08b57fe0f7
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2018-03-29 18:49:38 +0100

    Swap archer -> hector

--- epoch3d/src/infer_compiler.sh
+++ epoch3d/src/infer_compiler.sh
@@ -11,7 +11,7 @@ echo -n $MPIF90_VERS | grep -iE "ifort|intel" &> /dev/null
 if [ $? ==  0 ] ; then COMPILER='intel'; fi
 #Check for Cray, i.e. hector/archer
 ftn -V &> /dev/null
-if [ $? ==  0 ] ; then COMPILER='archer'; fi
+if [ $? ==  0 ] ; then COMPILER='hector'; fi
 #Check for ibm according to current Makefile
 mpixlf90_r &> /dev/null
 if [ $? ==  0 ] ; then COMPILER='ibm'; fi
