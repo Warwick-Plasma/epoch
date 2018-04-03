1ff818d6dabe3aeba31eeffab17689984b149d24
Keith Bennett <K.Bennett@warwick.ac.uk>
2018-04-03 16:02:57 +0100

    Tidied up infer_compiler.sh script

--- scripts/infer_compiler.sh
+++ scripts/infer_compiler.sh
@@ -1,19 +1,23 @@
-#!/bin/bash
+#! /bin/bash
 
 COMPILER=''
 
 MPIF90_VERS=`mpif90 --version 2>/dev/null`
-#check for gfortran
+
+# Check for gfortran
 echo -n $MPIF90_VERS | grep -iE "GNU|gfortran" &> /dev/null
-if [ $? ==  0 ] ; then COMPILER='gfortran'; fi
-#Check for intel
+[ $? == 0 ] && COMPILER='gfortran'
+
+# Check for intel
 echo -n $MPIF90_VERS | grep -iE "ifort|intel" &> /dev/null
-if [ $? ==  0 ] ; then COMPILER='intel'; fi
-#Check for Cray, i.e. hector/archer
+[ $? == 0 ] && COMPILER='intel'
+
+# Check for Cray, i.e. hector/archer
 ftn -V &> /dev/null
-if [ $? ==  0 ] ; then COMPILER='hector'; fi
-#Check for ibm according to current Makefile
+[ $? == 0 ] && COMPILER='hector'
+
+# Check for ibm according to current Makefile
 mpixlf90_r &> /dev/null
-if [ $? ==  0 ] ; then COMPILER='ibm'; fi
+[ $? == 0 ] && COMPILER='ibm'
 
 echo -n $COMPILER
