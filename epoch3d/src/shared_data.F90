848e1c4e62c14206579b77054e0eeb36dc1c5af1
Chris Brady <c.s.brady@warwick.ac.uk>
2018-08-21 13:01:07 +0100

    Added antenna to 3D

--- epoch3d/src/shared_data.F90
+++ epoch3d/src/shared_data.F90
@@ -456,6 +456,7 @@ MODULE shared_parser_data
   INTEGER, PARAMETER :: c_func_drifty = 45
   INTEGER, PARAMETER :: c_func_driftz = 46
   INTEGER, PARAMETER :: c_func_arctan2 = 47
+  INTEGER, PARAMETER :: c_func_in_range = 48
 
   INTEGER, PARAMETER :: c_func_custom_lowbound = 4096
 
