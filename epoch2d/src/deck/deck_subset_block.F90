8afc2010ddaac7cd423662856b752bbd76d6b443
Keith Bennett <k.bennett@warwick.ac.uk>
2018-11-19 13:37:49 +0000

    Change from_file_on_restart to file_overrides_restart
    This removes the "from_file_on_restart" filename parameter from the subset
    block and adds a logical flag "file_overrides_restart". The filename
    will be specified using "from_file".

--- epoch2d/src/deck/deck_subset_block.F90
+++ epoch2d/src/deck/deck_subset_block.F90
@@ -356,8 +356,7 @@ CONTAINS
       RETURN
     END IF
 
-    IF (str_cmp(element, 'from_file') &
-        .OR. str_cmp(element, 'from_file_on_restart')) THEN
+    IF (str_cmp(element, 'from_file')) THEN
 #if defined(PARTICLE_ID) || defined(PARTICLE_ID4)
       sub%persistent = .TRUE.
       sub%filename = TRIM(value)
@@ -370,6 +369,15 @@ CONTAINS
         errcode = c_err_bad_value
         RETURN
       END IF
+#else
+      errcode = c_err_pp_options_missing
+      extended_error_string = '-DPARTICLE_ID'
+#endif
+      RETURN
+    END IF
+
+    IF (str_cmp(element, 'file_overrides_restart')) THEN
+#if defined(PARTICLE_ID) || defined(PARTICLE_ID4)
       sub%add_after_restart = str_cmp(element, 'from_file_on_restart')
 #else
       errcode = c_err_pp_options_missing
