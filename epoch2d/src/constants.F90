f646a32198c73ecda8a60e4926104301fc048708
Keith Bennett <k.bennett@warwick.ac.uk>
2019-07-05 11:25:44 +0100

    Allow specifying boolean deck constants

--- epoch2d/src/constants.F90
+++ epoch2d/src/constants.F90
@@ -107,6 +107,9 @@ MODULE constants
   INTEGER, PARAMETER :: c_ds_first = 1
   INTEGER, PARAMETER :: c_ds_last = 2
 
+  INTEGER, PARAMETER :: c_true_value  = 123454321
+  INTEGER, PARAMETER :: c_false_value = -123454321
+
   ! IO codes
   INTEGER, PARAMETER :: c_io_none = 0
   INTEGER, PARAMETER :: c_io_always = 2**0
@@ -344,6 +347,8 @@ MODULE constants
   INTEGER, PARAMETER :: c_const_pico = 14
   INTEGER, PARAMETER :: c_const_femto = 15
   INTEGER, PARAMETER :: c_const_atto = 16
+  INTEGER, PARAMETER :: c_const_true = 17
+  INTEGER, PARAMETER :: c_const_false = 18
 
   ! Constants refering to grid properties
   INTEGER, PARAMETER :: c_const_xb = 22
